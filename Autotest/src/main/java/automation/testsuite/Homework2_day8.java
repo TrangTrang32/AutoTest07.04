e1f45a6c57da0963001b6e2752833fde8021d355
unknown <phamthutrang03022000@gmail.com>
2024-05-21 22:56:58 +0700

    Phạm Trang update code bài tập 2 day 8

--- Autotest/src/main/java/automation/testsuite/Homework2_day8.java
+++ Autotest/src/main/java/automation/testsuite/Homework2_day8.java
@@ -16,9 +16,9 @@ public class Homework2_day8 extends CommonBase {
 		WebElement nameId = driver.findElement(By.id("name"));
 		System.out.println("Name Id Element is: "+ nameId);
 		WebElement addressId = driver.findElement(By.id("address"));
-		System.out.println("Password Id Element is: "+ addressId);
+		System.out.println("Address Id Element is: "+ addressId);
 		WebElement emailId = driver.findElement(By.id("email"));
-		System.out.println("Password Id Element is: "+ emailId);
+		System.out.println("Email Id Element is: "+ emailId);
 		WebElement passwordId = driver.findElement(By.id("password"));
 		System.out.println("Password Id Element is: "+ passwordId);
 	}
@@ -27,18 +27,18 @@ public class Homework2_day8 extends CommonBase {
 		WebElement nameName = driver.findElement(By.name("name"));
 		System.out.println("Name Name Element is: "+ nameName);
 		WebElement addressName = driver.findElement(By.name("address"));
-		System.out.println("Password Name Element is: "+ addressName);
+		System.out.println("Address Name Element is: "+ addressName);
 		WebElement emailName = driver.findElement(By.name("email"));
-		System.out.println("Password Id Element is: "+ emailName);
+		System.out.println("Email Name Element is: "+ emailName);
 		WebElement passwordName = driver.findElement(By.name("password"));
-		System.out.println("Mobile Number Name Element is: "+ passwordName);
+		System.out.println("Password Name Element is: "+ passwordName);
 	}
 	@Test
 	public void tagNameElement() {
 		WebElement inputTagname = driver.findElement(By.tagName("input"));
 		System.out.println("Input Element is: "+ inputTagname);
 		WebElement textareaTagName = driver.findElement(By.tagName("textarea"));
-		System.out.println("Input Element is: "+ textareaTagName);
+		System.out.println("Textarea Element is: "+ textareaTagName);
 	}
 
 }
